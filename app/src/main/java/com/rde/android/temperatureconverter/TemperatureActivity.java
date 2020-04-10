f471c5d52f7be9432331882bc9f5bc767634be29
Rodolfo d'Ettorre <rodolfothegreat@hotmail.com>
2020-04-10 21:10:39 +1000

    Upgrading to androidx

--- app/src/main/java/com/rde/android/temperatureconverter/TemperatureActivity.java
+++ app/src/main/java/com/rde/android/temperatureconverter/TemperatureActivity.java
@@ -1,10 +1,10 @@
 package com.rde.android.temperatureconverter;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -95,6 +95,7 @@ public class TemperatureActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            showToast("Me Settings");
             return true;
         }
 
